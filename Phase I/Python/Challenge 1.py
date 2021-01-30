793c143e6d5c1b31f618b5a866f65f27a19df8d0
LionelRohner <64872630+LionelRohner@users.noreply.github.com>
2021-01-30 19:08:34 +0100

    found error

--- Phase I/Python/Challenge 1.py
+++ Phase I/Python/Challenge 1.py
@@ -1,32 +1,39 @@
 import numpy as np
+import math
 import timeit
 import time
 
-A = np.arange(0, (10 ** 6), 1)
-# A = np.asarray(range(0,10**6,1))
-
-print(sum(A[np.where((A % 3 == 0) | (A % 5 == 0))]))
+A = np.arange(1, (1 + 10 ** 6), 1)
 
 
-# # timing method?
-# def main():
-#     A = np.arange(0,10**6,1)
-#     # A = np.asarray(range(0,10**6,1))
+A = np.array([i for i in range(1,10**6) if i % 3 == 0 or i % 5 == 0], dtype=np.int64).sum()
+
+print(A)
 #
-#     print(np.sum(A[np.where( (A % 3 == 0) | (A % 5 == 0))]))
-#
-#
-# # timing method 1
-# code_block = """
-# import numpy as np
-# A = np.arange(0,10**6,1)
-#
-# print(np.sum(A[np.where( (A % 3 == 0) | (A % 5 == 0))]))
-# """
-# elapsed_time = timeit.timeit(code_block, number=10)
-# print(np.mean(elapsed_time))
-#
-# # timing method two
-# start_time = time.time()
-# main()
-# print("--- %s seconds ---" % (time.time() - start_time))
+
+
+# timing method 1
+code_block = """
+import numpy as np
+import math
+
+A = np.arange(0,10**6,1)
+
+math.fsum(A[np.where( (A % 3 == 0) | (A % 5 == 0))])
+"""
+elapsed_time = timeit.timeit(code_block, number=10)
+print(np.mean(elapsed_time))
+
+
+
+
+# timing method?
+def main():
+    A = np.arange(0,10**6,1)
+    # A = np.asarray(range(0,10**6,1))
+
+    print(math.fsum(A[np.where( (A % 3 == 0) | (A % 5 == 0))]))
+# timing method two
+start_time = time.time()
+main()
+print("--- %s seconds ---" % (time.time() - start_time))
