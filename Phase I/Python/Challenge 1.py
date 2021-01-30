bcd71cdeaa6e21912b7c225c2c39a525d6c7d5ad
LionelRohner <64872630+LionelRohner@users.noreply.github.com>
2021-01-30 18:51:41 +0100

    python vs r
    different results????

--- Phase I/Python/Challenge 1.py
+++ Phase I/Python/Challenge 1.py
@@ -2,27 +2,31 @@ import numpy as np
 import timeit
 import time
 
+A = np.arange(0, (10 ** 6), 1)
+# A = np.asarray(range(0,10**6,1))
 
-# timing method?
-
-def main():
-    A = np.arange(0,10**6,1)
-    # A = np.asarray(range(0,10**6,1))
-
-    print(np.sum(A[np.where( (A % 3 == 0) | (A % 5 == 0))]))
+print(sum(A[np.where((A % 3 == 0) | (A % 5 == 0))]))
 
 
-# timing method 1
-code_block = """
-import numpy as np
-A = np.arange(0,10**6,1)
-
-print(np.sum(A[np.where( (A % 3 == 0) | (A % 5 == 0))]))
-"""
-elapsed_time = timeit.timeit(code_block, number=10)
-print(np.mean(elapsed_time))
-
-# timing method two
-start_time = time.time()
-main()
-print("--- %s seconds ---" % (time.time() - start_time))
+# # timing method?
+# def main():
+#     A = np.arange(0,10**6,1)
+#     # A = np.asarray(range(0,10**6,1))
+#
+#     print(np.sum(A[np.where( (A % 3 == 0) | (A % 5 == 0))]))
+#
+#
+# # timing method 1
+# code_block = """
+# import numpy as np
+# A = np.arange(0,10**6,1)
+#
+# print(np.sum(A[np.where( (A % 3 == 0) | (A % 5 == 0))]))
+# """
+# elapsed_time = timeit.timeit(code_block, number=10)
+# print(np.mean(elapsed_time))
+#
+# # timing method two
+# start_time = time.time()
+# main()
+# print("--- %s seconds ---" % (time.time() - start_time))
