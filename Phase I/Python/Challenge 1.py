0d3bbf3a6c0ba18fb4ce766fe20ffb2f4307225f
LionelRohner <64872630+LionelRohner@users.noreply.github.com>
2021-01-30 18:42:20 +0100

    Update
    Started with python

---
+++ Phase I/Python/Challenge 1.py
@@ -0,0 +1,28 @@
+import numpy as np
+import timeit
+import time
+
+
+# timing method?
+
+def main():
+    A = np.arange(0,10**6,1)
+    # A = np.asarray(range(0,10**6,1))
+
+    print(np.sum(A[np.where( (A % 3 == 0) | (A % 5 == 0))]))
+
+
+# timing method 1
+code_block = """
+import numpy as np
+A = np.arange(0,10**6,1)
+
+print(np.sum(A[np.where( (A % 3 == 0) | (A % 5 == 0))]))
+"""
+elapsed_time = timeit.timeit(code_block, number=10)
+print(np.mean(elapsed_time))
+
+# timing method two
+start_time = time.time()
+main()
+print("--- %s seconds ---" % (time.time() - start_time))
