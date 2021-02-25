e7cb0f5ba16a1f12c7c3369bf2a3d604ced60c46
LionelRohner <64872630+LionelRohner@users.noreply.github.com>
2021-02-25 13:35:04 +0100

    Update

--- Phase II/Python/Challenge_2.py
+++ Phase II/Python/Challenge_2.py
@@ -15,7 +15,7 @@ res = pd.DataFrame({
 A = np.array([1,1,1,0], dtype=object).reshape(2,2)
 
 
-def fibMatrix(n):
+def fibMatrixEven(n):
     lst = []
     # sum = 0
     # standard loop over each Fibonacci Number up to 4 Mio
@@ -28,7 +28,34 @@ def fibMatrix(n):
     return(lst)
 
 
+n = 10
+print(fibMatrixEven(n))
+print(sum(fibMatrixEven(n)))
 
+# find n
+k = (11//3)+2
+
+
+A = np.array([1,1,1,0], dtype=object).reshape(2,2)
+I = np.array([1,0,1,0], dtype=object).reshape(2,2)
+
+F3n_3 = np.linalg.matrix_power(A,n+2)
+
+
+
+# print((np.matmul((F3n_3-I),(F3n_3-I))//2)[1,1])
+print((F3n_3[1,1]-1)//2)
+
+# my algo 1
+quit()
+A = np.array([1,1,1,0], dtype=object).reshape(2,2)
+Fk = np.linalg.matrix_power(A, ((n+1)))
+
+# F_k        F_k+1      F_k
+print(((Fk[0,1]*(2*Fk[0, 0] - Fk[0, 1]))-1)//2)
+
+
+quit()
 
 algo1 = """
 import numpy as np
@@ -49,23 +76,21 @@ elapsed_time = tmt.timeit(algo1, number=nIter)
 print("algo1 ", (elapsed_time/nIter))
 print("Loop Done.")
 
-# Recursive
 
-
-algo2 = """
-import numpy as np
-
-A = np.array([1,1,1,0], dtype=object).reshape(2,2)
-np.linalg.matrix_power(A, 7999996)[0, 1] - 1
-"""
-
-# tst2 = [tmt.timeit(stmt=algo2, number=i) for i in range(5000)]
-# res["Algo2"] = tst2
-# print(np.mean(tst2))
-
-elapsed_time = tmt.timeit(algo2, number=nIter)
-print("algo2: ", (elapsed_time/nIter))
-print("Loop Done.")
+# algo2 = """
+# import numpy as np
+#
+# A = np.array([1,1,1,0], dtype=object).reshape(2,2)
+# np.linalg.matrix_power(A, 7999996)[0, 1] - 1
+# """
+#
+# # tst2 = [tmt.timeit(stmt=algo2, number=i) for i in range(5000)]
+# # res["Algo2"] = tst2
+# # print(np.mean(tst2))
+#
+# elapsed_time = tmt.timeit(algo2, number=nIter)
+# print("algo2: ", (elapsed_time/nIter))
+# print("Loop Done.")
 
 # algoCrap = """
 # import numpy as np
@@ -117,7 +142,28 @@ elapsed_time = tmt.timeit(algo3, number=nIter)
 print("algo3: ", elapsed_time/nIter)
 print("Loop Done.")
 
-print(res)
+
+karatsuba(np.matmul(Fk[0,0:1],[[2],[-1]]),Fk, )
+
+algo4 = """
+import numpy as np
+A = np.array([1,1,1,0], dtype=object).reshape(2,2)
+Fk = np.linalg.matrix_power(A, 1999999)
+
+
+# F_k        F_k+1      F_k
+(Fk[0,1]*(2*Fk[0, 0] - Fk[0, 1]))-1
+"""
+
+# tst1 = [tmt.timeit(stmt=algo1, number=i) for i in range(5000)]
+# res["Algo1"] = tst1
+# print(np.mean(tst1))
+
+nIter = 11
+
+elapsed_time = tmt.timeit(algo1, number=nIter)
+print("algo1 ", (elapsed_time/nIter))
+print("Loop Done.")
 
 algoComp = """
 #### Comparison
@@ -160,6 +206,51 @@ elapsed_time = tmt.timeit(algoComp, number=nIter)
 print("algoComp: ", elapsed_time/nIter)
 print("Loop Done.")
 
+
+algoComp2 = """
+
+import numpy as np
+
+# Karatsuba fast multiplication algorithm (Python)
+# 
+# Copyright (c) 2020 Project Nayuki
+# All rights reserved. Contact Nayuki for licensing.
+# https://www.nayuki.io/page/karatsuba-multiplication
+# 
+
+
+# Requirement: _CUTOFF >= 64, or else there will be infinite recursion.
+_CUTOFF = 1536
+
+
+def multiply(x, y):
+    if x.bit_length() <= _CUTOFF or y.bit_length() <= _CUTOFF:  # Base case
+        return x * y
+    
+    else:
+        n = max(x.bit_length(), y.bit_length())
+        half = (n + 32) // 64 * 32
+        mask = (1 << half) - 1
+        xlow = x & mask
+        ylow = y & mask
+        xhigh = x >> half
+        yhigh = y >> half
+        
+        a = multiply(xhigh, yhigh)
+        b = multiply(xlow + xhigh, ylow + yhigh)
+        c = multiply(xlow, ylow)
+        d = b - a - c
+        return (((a << half) + d) << half) + c
+
+
+A = np.array([1,1,1,0], dtype=object).reshape(2,2)
+Fk = np.linalg.matrix_power(A, 1999999)
+
+multiply(x = Fk[0,1], y = (2*Fk[0, 0] - Fk[0, 1])) - 1 
+"""
+elapsed_time = tmt.timeit(algoComp2, number=nIter)
+print("algoComp2: ", elapsed_time/nIter)
+print("Loop Done.")
 quit()
 
 
@@ -187,5 +278,4 @@ print("done")
 """
 
 
-#
-
+print(res)
--- Phase II/Python/test.py
+++ Phase II/Python/test.py
@@ -2,6 +2,28 @@ import numpy as np
 import decimal
 
 
+import math
+
+_1_50 = 1 << 50  # 2**50 == 1,125,899,906,842,624
+
+def isqrt(x):
+    """Return the integer part of the square root of x, even for very
+    large integer values."""
+    if x < 0:
+        raise ValueError('square root not defined for negative numbers')
+    if x < _1_50:
+        return int(math.sqrt(x))  # use math's sqrt() for small parameters
+    n = int(x)
+    if n <= 1:
+        return n  # handle sqrt(0)==0, sqrt(1)==1
+    # Make a high initial estimate of the result (a little lower is slower!!!)
+    r = 1 << ((n.bit_length() + 1) >> 1)
+    while True:
+        newr = (r + n // r) >> 1  # next estimate by Newton-Raphson
+        if newr >= r:
+            return r
+        r = newr
+
 def karatsuba(x , y, m):
     """
 
@@ -28,16 +50,26 @@ def karatsuba(x , y, m):
     return(a*m**2+(b-a-c)*m+c)
 
 
-# how bis is it
+
 A = np.array([1,1,1,0], dtype=object).reshape(2,2)
-blin = np.linalg.matrix_power(A, 3)[0,0]
-print(blin)
+Fk = np.linalg.matrix_power(A, 10+2)
+print(Fk[0,0]-1)
+print(Fk)
 quit()
-A = np.array([1,1,1,0], dtype=object).reshape(2,2)
-blin = np.linalg.matrix_power(A, (4*10**6-2))[0, 0]
+print((np.dot(Fk[0,:],[[2],[-1]])*Fk[0,0])-1)
+# karatsuba(np.matmul(Fk[0,0:1],[[2],[-1]]),Fk[0,1])
+
+quit()
+blin = (2*Fk[0, 0] - Fk[0, 1])
+
 print("printing")
-# d = decimal.Decimal(blin)
-# print(format(d, '.6e'))
+d = decimal.Decimal(blin)
+print(format(d, '.6e'))
 print(blin%2)
 
-print(4*10**6-2)
+print("printing sqrt")
+sqrtBlin = isqrt(blin)
+dd = decimal.Decimal(sqrtBlin)
+print(format(dd, '.6e'))
+
+# dude sqrt(Fn) is 3.433651e+208987.... that should be m for karatsuba
