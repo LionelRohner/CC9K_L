a5a6f99d6e8e87269ff23bd95b7787598708492b
LionelRohner <64872630+LionelRohner@users.noreply.github.com>
2021-03-11 13:03:50 +0100

    Clean and Start Challenge 3

---
+++ Phase III/Python/BenchPress.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import timeit as tmt
+import matplotlib.pyplot as plt
+import os
+
+
+class BenchPress:
+
+    def __init__(self, data, benchMeth):
+
+        # create result Pandas
+        self.res = pd.DataFrame(data)
+        self.benchMeth = benchMeth
+
+    def getDaReps(self, algo, algoN, nIter = None):
+
+
+        if self.benchMeth == "Nenad":
+            if nIter == None:
+                nIter = 100
+
+            tst = [tmt.timeit(stmt=algo, number=i) for i in range(nIter)]
+            self.res[algoN] = tst
+            print()
+        else:
+            if nIter == None:
+                nIter = 5000
+            elapsed_time = tmt.timeit(algo, number=nIter)
+            print(algoN, ": ", elapsed_time / nIter)
+            self.res[algoN] = [elapsed_time]
+            print("Loop Done.")
+            print()
+
+    def dickPic(self):
+
+        # depending on the method show propagation, i.e. the Nenadian Plot
+        if self.benchMeth == "Nenad":
+            self.res.plot(ylim=[min(self.res.min()), max(self.res.max())])
+
+        # Crappy boxplot, whose ylim makes you want to jump out of the window
+        else:
+            self.res.boxplot()
+
+        plt.show()
+
+
+
+    def returnRes(self):
+        return(self.res)
+
+    def saveCSV(self, outName = '\\res.csv'):
+            self.res.to_csv(os.getcwd() + outName)
--- Phase III/Python/Challenge_3.py
+++ Phase III/Python/Challenge_3.py
@@ -1,20 +1,64 @@
+###################################################################################################################
+# Modules
+
 import numpy as np
 import random as rd
-import timeit as tm
+import BenchPress as bp
+
 
 ###################################################################################################################
+# BenchMarkus
+
+data = {
+    "Algo1": [],
+    "Algo2": []
+}
+
+# benchMarkus = bp.BenchPress(data, "Nenad")
+benchMarkus = bp.BenchPress(data = data, benchMeth = "Lionel")
 
 
-a = np.random.random(4)
-print(np.where(a < 0.5, rd.choice("AT"), rd.choice("GC")))
-print(np.random.random(4))
+###################################################################################################################
+# Algo 1
+
+
+# x = 3272116950/80 = 40901461.875 ~ 40901462
+
+# print(3272116950/80)
+
+n = 80
+
+np.random.random(4000000000).astype('f2').reshape(100000,40000)
+quit()
+# quit()
+# preAlloc = np.random.random(80)
+# seqs = np.where(preAlloc < 0.5, rd.choice("AT"), rd.choice("GC"))
+# print(np.random.random(4))
 
 
 
 algoN = "Algo1"
 
 algo = """
+import numpy as np
 
+print(np.random.random(3272116950).reshape(40901462,80))
 """
 
+benchMarkus.getDaReps(algo = algo, algoN = algoN)
+print(benchMarkus.returnRes())
 
+quit()
+
+###################################################################################################################
+# Algo 1
+
+algoN = "Algo2"
+
+algo = """
+a = np.random.random(4)
+print(np.where(a < 0.5, rd.choice("AT"), rd.choice("GC")))
+print(np.random.random(4))
+"""
+
+benchMarkus.getDaReps(algo = algo, algoN = algoN)
