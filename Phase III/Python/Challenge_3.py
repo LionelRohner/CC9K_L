0e405af775b595e4f5a909c4f6c6e2fa61cefbf3
LionelRohner <64872630+LionelRohner@users.noreply.github.com>
2021-03-19 11:26:41 +0100

    Challenge 3
    Idea kinda working, but now idea how efficient it is

--- Phase III/Python/Challenge_3.py
+++ Phase III/Python/Challenge_3.py
@@ -10,55 +10,132 @@ import BenchPress as bp
 # BenchMarkus
 
 data = {
-    "Algo1": [],
-    "Algo2": []
+    "A1": [],
+    "A2": [],
+    "A3": [],
+    "A4": []
 }
 
-# benchMarkus = bp.BenchPress(data, "Nenad")
 benchMarkus = bp.BenchPress(data = data, benchMeth = "Lionel")
 
+preAlloc = np.random.random(10).reshape(1, 10)
 
-###################################################################################################################
-# Algo 1
+# Create boolean masks
+A = preAlloc < 0.25
+T = ((preAlloc > 0.25) & (preAlloc < 0.5))
+G = ((preAlloc > 0.5) & (preAlloc < 0.75))
+C = ((preAlloc > 0.75) & (preAlloc < 1))
+
+# Project boolean mask to random number array
+seq = np.where(A,"A",np.where(T,"T", np.where(G, "G", np.where(C,"C",0))))
+print(seq)
 
 
-# x = 3272116950/80 = 40901461.875 ~ 40901462
-
-# print(3272116950/80)
-
-n = 80
-
-np.random.random(4000000000).astype('f2').reshape(100000,40000)
 quit()
+# dists[(np.where((dists >= r) & (dists <= r + dr)))]
+
+
+seqs = np.where(preAlloc < 0.25, "A", preAlloc)
+print(seqs)
+seqs2 = np.where(seqs < 0.5 , "T", seqs)
+print(seqs)
+seqs3 = np.where(seqs2 < 0.75, "G", "C")
+print(seqs)
+quit()
+# import numpy as np
+# c = 80
+# it = 2
+# total = 8000
+# r = (total // c) // it
+#
+# f=open('HG1.fasta','a')
+# for i in range(0, it):
+#     preAlloc = np.random.random(total//it).reshape(r,c)
+#     seqs = np.where(preAlloc < 0.5, rd.choice("AT"), rd.choice("GC"))
+#     np.savetxt(f, seqs, delimiter="", fmt="%s")
 # quit()
-# preAlloc = np.random.random(80)
-# seqs = np.where(preAlloc < 0.5, rd.choice("AT"), rd.choice("GC"))
-# print(np.random.random(4))
 
-
-
-algoN = "Algo1"
-
+algoN = "A1"
 algo = """
 import numpy as np
+import random as rd
 
-print(np.random.random(3272116950).reshape(40901462,80))
+c = 80
+it = 1
+total = 8000
+r = (total // c) // it
+
+f=open('HG1.fasta','a')
+for i in range(0, it):
+    preAlloc = np.random.random(total//it).reshape(r,c)
+    seqs = np.where(preAlloc < 0.5, rd.choice("AT"), rd.choice("GC"))
+    np.savetxt(f, seqs, delimiter="", fmt="%s")
+
 """
 
 benchMarkus.getDaReps(algo = algo, algoN = algoN)
-print(benchMarkus.returnRes())
 
-quit()
+#############
 
-###################################################################################################################
-# Algo 1
-
-algoN = "Algo2"
-
+algoN = "A2"
 algo = """
-a = np.random.random(4)
-print(np.where(a < 0.5, rd.choice("AT"), rd.choice("GC")))
-print(np.random.random(4))
+import numpy as np
+import random as rd
+
+c = 80
+it = 2
+total = 8000
+r = (total // c) // it
+
+f=open('HG2.fasta','a')
+for i in range(0, it):
+    preAlloc = np.random.random(total//it).reshape(r,c)
+    seqs = np.where(preAlloc < 0.5, rd.choice("AT"), rd.choice("GC"))
+    np.savetxt(f, seqs, delimiter="", fmt="%s")
+"""
+
+benchMarkus.getDaReps(algo = algo, algoN = algoN)
+
+##################
+
+algoN = "A3"
+algo = """
+import numpy as np
+import random as rd
+
+c = 80
+it = 4
+total = 8000
+r = (total // c) // it
+
+f=open('HG3.fasta','a')
+for i in range(0, it):
+    preAlloc = np.random.random(total//it).reshape(r,c)
+    seqs = np.where(preAlloc < 0.5, rd.choice("AT"), rd.choice("GC"))
+    np.savetxt(f, seqs, delimiter="", fmt="%s")
+
+"""
+
+benchMarkus.getDaReps(algo = algo, algoN = algoN)
+
+#####################
+
+algoN = "A4"
+algo = """
+import numpy as np
+import random as rd
+
+c = 80
+it = 8
+total = 8000
+r = (total // c) // it
+
+f=open('HG4.fasta','a')
+for i in range(0, it):
+    preAlloc = np.random.random(total//it).reshape(r,c)
+    seqs = np.where(preAlloc < 0.5, rd.choice("AT"), rd.choice("GC"))
+    np.savetxt(f, seqs, delimiter="", fmt="%s")
+
 """
 
 benchMarkus.getDaReps(algo = algo, algoN = algoN)
