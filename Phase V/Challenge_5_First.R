57e9bc9a8c87c8fac7e35629e5d698ec2f51a99f
LionelRohner <64872630+LionelRohner@users.noreply.github.com>
2021-05-25 11:16:30 +0200

    Displaying The Matrix Is Now Much Faster

--- Phase V/Challenge_5_First.R
+++ Phase V/Challenge_5_First.R
@@ -20,6 +20,7 @@ library(rbenchmark)
 # main
 challenge_V <- function(countOnly = T, upperLim = 1e6, n, r){
   
+  # ONLY COUNT THE NUMBER OF BINOM COEF THAT ARE BIGGER THAN 1e6  
   if(countOnly){
     
     # create an upper triangular matrix, because r should be smaller than n
@@ -31,20 +32,58 @@ challenge_V <- function(countOnly = T, upperLim = 1e6, n, r){
     
     # vectorized binom coefficient calculation for all indeces of interest.
     return(length(which(choose(idx[,"col"],idx[,"row"]) > upperLim)))
+  
+  # DISPLAY RESULTS  
   } else {
     
-    # create an upper triangular matrix, because r should be smaller than n
     # Here diagonal is computed to avoid confusion of 0s on the diagonal
     A <- upper.tri(matrix(NA,nrow = n, ncol = r), diag = T)
+    idx <- which(A == T, arr.ind = T)
+    
+    # pre-allocate results into a vector
+    res <- choose(idx[,"col"],idx[,"row"])
+    
+    # loop through indeces pairs. Didnt find a way to drop the loop
+    # DOES NOT WORK : A[idx[,"row"],idx[,"col"]] <- res[]
+    for (i in 1:nrow(idx)){
+      A[idx[i,"row"],idx[i,"col"]] <- res[i]
+    }
+    return(t(A))
+  } 
+}
+
+
+challenge_V_v2 <- function(countOnly = T, upperLim = 1e6, n, r){
+  
+  # ONLY COUNT THE NUMBER OF BINOM COEF THAT ARE BIGGER THAN 1e6  
+  if(countOnly){
+    
+    # create an upper triangular matrix, because r should be smaller than n
+    # Moreover, the diagonal can be excluded as choose(n,n) is always one
+    A <- upper.tri(matrix(NA,nrow = n, ncol = r), diag = F)
     
     # Output array indeces (matrix of row and col indeces), where TRUE
     idx <- which(A == T, arr.ind = T)
-    for (i in idx[,"row"]){
-      for (j in idx[,"col"]){
-        A[i,j] <- choose(j,i)
-      }
+    
+    # vectorized binom coefficient calculation for all indeces of interest.
+    return(length(which(choose(idx[,"col"],idx[,"row"]) > upperLim)))
+    
+    # DISPLAY RESULTS  
+  } else {
+    
+    # Here diagonal is computed to avoid confusion of 0s on the diagonal
+    A <- upper.tri(matrix(NA,nrow = n, ncol = r), diag = T)
+    idx <- which(A == T, arr.ind = T)
+    
+    # pre-allocate results into a vector
+    res <- choose(idx[,"col"],idx[,"row"])
+    
+    # loop through indeces pairs. Didnt find a way to drop the loop
+    # DOES NOT WORK : A[idx[,"row"],idx[,"col"]] <- res[]
+    for (i in 1:nrow(idx)){
+      A[idx[i,"row"],idx[i,"col"]] <- res[i]
     }
-    return(A)
+    return(t(A))
   } 
 }
 
@@ -54,40 +93,25 @@ challenge_V <- function(countOnly = T, upperLim = 1e6, n, r){
 
 # Example 1
 choose(5,3)
+challenge_V(n = 100, r = 100, countOnly = F)[5,3]
 
 # Example 2
 choose(23,10)
-
-n = r = 10
-
-# create an upper triangular matrix, because r should be smaller than n
-# Moreover, the diagonal can be excluded as choose(n,n) is always one
-A <- upper.tri(matrix(NA,nrow = n, ncol = r), diag = F)
-
-# Output array indeces (matrix of row and col indeces), where TRUE
-idx <- which(A == T, arr.ind = T)
-
-for (i in idx[,1]){
-  for (j in idx[,2]){
-    A[i,j] <- choose(i,j)
-  }
-}
-
-nrow(which(A > 1e6, arr.ind = T))
+challenge_V(n = 100, r = 100, countOnly = F)[23,10]
 
 
 #------------------------------------------------------------------------------#
 # Code -------------------------------------------------------------------
 #------------------------------------------------------------------------------#
 
-challenge_V(n = 100, r = 100, countOnly = F)
+challenge_V(n = 100, r = 100, countOnly = T)
 
 #------------------------------------------------------------------------------#
 # Benchmark ---------------------------------------------------------------
 #------------------------------------------------------------------------------#
 
 test <- benchmark("Algo1" = {challenge_V(n = 100, r = 100)},
-                  "Algo1_loops" = {challenge_V(n = 100, r = 100, countOnly = F)},
+                  "Algo1_Display" = {challenge_V(n = 100, r = 100, countOnly = F)},
                   replications = 10)
 
 
